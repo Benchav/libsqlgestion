29034c661c223a26fd7e41a531da4bc9587b9d8b
benchav <joshua44benja@gmail.com>
2026-05-04 12:58:38 -0600

    fix: specify column types for revokedAt and expiresAt in Session entity

--- backend/dist/domain/entities/Session.js
+++ backend/dist/domain/entities/Session.js
@@ -30,11 +30,11 @@ __decorate([
     __metadata("design:type", String)
 ], Session.prototype, "refreshTokenHash", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ nullable: true }),
+    (0, typeorm_1.Column)({ type: 'datetime', nullable: true }),
     __metadata("design:type", Object)
 ], Session.prototype, "revokedAt", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ nullable: true }),
+    (0, typeorm_1.Column)({ type: 'datetime', nullable: true }),
     __metadata("design:type", Object)
 ], Session.prototype, "expiresAt", void 0);
 __decorate([
